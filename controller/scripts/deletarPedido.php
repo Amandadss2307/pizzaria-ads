5a0e0177a8f740dcf0ad1da8c0acbc313b214c4c
Amanda <amandaasantos2307@gmail.com>
2023-10-25 19:09:51 -0300

    Adicionando a listagem de pedidos que apenas o usuário pode ver e de criação de pedidos

---
+++ controller/scripts/deletarPedido.php
@@ -0,0 +1,22 @@
+<?php
+session_start();
+require '../../utils/session.php';
+require '../connections/connection.php';
+include '../../utils/verifyAdminUser.php';
+
+if (!verifyAdminUser()) {
+  header('Location: ../../pages/pedidos.php');
+} else {
+  $id = $_GET['id'];
+
+  $delete = "DELETE FROM pedido WHERE `pedido`.`id` = $id";
+
+  $conn->query($delete);
+
+  $conn->close();
+
+  echo "<script>
+    window.alert('Pedido excluído com sucesso!')
+    window.location.href='../../pages/pedidos/listagem.php';
+  </script>";
+}
