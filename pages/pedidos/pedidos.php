2cac9ede901994460c585efa3a2806ad01698f39
Amanda <amandaasantos2307@gmail.com>
2023-10-25 19:59:47 -0300

    Adicionando código da página de enviar pedidos

--- pages/pedidos/pedidos.php
+++ pages/pedidos/pedidos.php
@@ -3,6 +3,16 @@ session_start();
 require '../../utils/session.php';
 require('../../controller/connections/connection.php');
 
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $produto = $_POST['produto'];
+    $quantidade = $_POST['quantidade'];
+    $observacoes = $_POST['observacoes'];
+
+    echo "Pedido Recebido:<br>";
+    echo "Produto: $produto<br>";
+    echo "Quantidade: $quantidade<br>";
+    echo "Observações: $observacoes<br>";
+}
 ?>
 
 <!DOCTYPE html>
@@ -10,10 +20,21 @@ require('../../controller/connections/connection.php');
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title> Faça seu pedido</title>
+    <title>Faça seu pedido</title>
 </head>
 <body>
-    <h1> Faça seu pedido </h1>
-    
+    <h1>Faça seu pedido</h1>
+    <form method="POST" action="<?php echo $_SERVER['PHP_SELF']; ?>">
+        <label for="produto">Produto:</label>
+        <input type="text" name="produto" id="produto" required><br>
+
+        <label for="quantidade">Quantidade:</label>
+        <input type="number" name="quantidade" id="quantidade" required><br>
+
+        <label for="observacoes">Observações:</label>
+        <textarea name="observacoes" id="observacoes"></textarea><br>
+
+        <input type="submit" value="Enviar Pedido">
+    </form>
 </body>
 </html>
