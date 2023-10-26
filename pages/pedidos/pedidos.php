cad77625d640fd7443d3634fae9e0bc199891ad3
Amanda <amandaasantos2307@gmail.com>
2023-10-26 12:04:13 -0300

    Alterações em pedidos

--- pages/pedidos/pedidos.php
+++ pages/pedidos/pedidos.php
@@ -6,6 +6,9 @@ require('../../controller/connections/connection.php');
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $produto = $_POST['produto'];
     $quantidade = $_POST['quantidade'];
+    $endereco_entrega = $_POST['endereco_entrega'];
+    $forma_pagamento = $_POST['forma_pagamento'];
+    $tipo_entrega = $_POST['tipo_entrega'];
     $observacoes = $_POST['observacoes'];
 
     echo "Pedido Recebido:<br>";
@@ -25,11 +28,20 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 <body>
     <h1>Faça seu pedido</h1>
     <form action="../../controller/scripts/cadastroPedido.php" method="POST">
-        <label for="produto">Produto:</label>
-        <input type="text" name="produto" id="produto" required><br>
+        <label for="pedido">Produto:</label>
+        <input type="text" name="pedido" id="pedido" required><br>
 
         <label for="quantidade">Quantidade:</label>
         <input type="number" name="quantidade" id="quantidade" required><br>
+
+        <label for="endereço de entrega"> Endereço:</label>
+        <textarea name="endereço de entrega" id="endereco_entrega"></textarea><br>
+
+        <label for="forma de pagamento">Forma de pagamento:</label>
+        <input type="number" name="forma de pagamento" id="forma_pagamento" required><br>
+
+        <label for="tipo de entrega">Entrega ou retirada:</label>
+        <input type="text" name="entrega ou retirada" id="tipo_entrega" required><br>
 
         <label for="observacoes">Observações:</label>
         <textarea name="observacoes" id="observacoes"></textarea><br>
